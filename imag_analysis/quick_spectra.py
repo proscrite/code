a4203b589810f0afc28a7d5ce513db1ee6528290
proscrite <pablogfr94@gmail.com>
2025-02-19 15:43:03 +0200

    Dark image subtraction in quick_spectra

--- imag_analysis/quick_spectra.py
+++ imag_analysis/quick_spectra.py
@@ -8,42 +8,13 @@ from glob import glob
 import matplotlib
 matplotlib.rcParams.update({'errorbar.capsize': 4})
 
+from image_processing import find_fov, offset_circular_mask
+
 # filtAv = [438, 472, 500, 527, 549, 561, 568, 605, 631, 661, 692]
 # filtUn = np.array([28, 35, 29, 22, 21, 21, 26, 22, 28, 26, 47]) / 2
 filtAv = [438, 472, 549, 575, 586, 605, 631, 661, 676, 692]
 filtUn = np.array([28, 35, 21, 35, 26, 22, 28, 26, 29, 47]) / 2
 filtQE = np.array([0.52654585, 0.61510198, 0.67157561, 0.70026282, 0.71884383, 0.72505143, 0.72530424, 0.71414267, 0.68976776, 0.6438945])
-
-def prepare_spectrum(files, roi):
-    nfilt = len(files)
-    
-    img = io.imread(files[0]).astype(np.int64)
-    imroi = img[roi[0]:roi[1], roi[2]:roi[3]]
-    roiSize = imroi.shape[0] * imroi.shape[1]
-    offset = 100 * roiSize
-
-    avInt = []
-    unInt = []
-    for i in range(nfilt-1):
-        img = io.imread(files[i]).astype(np.int64)
-        imroi = img[roi[0]:roi[1], roi[2]:roi[3]]
-        intPx = (imroi.sum() - offset) / roiSize
-        avInt.append(intPx)
-        
-        unPx = np.sqrt((imroi**2).sum()) / roiSize
-        unInt.append(unPx)
-
-    avInt = np.array(avInt)
-    unInt = np.array(unInt)
-    cntQe = avInt / filtQE
-    # cntQe /= (filtUn*2)
-
-    unInt /= filtQE
-    # unInt /= (filtUn*2)
-
-
-    return cntQe, unInt
-
 
 def prepare_spectrum(files, roi):
     nfilt = len(files)
